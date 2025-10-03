7ce8b2f2b847ca2794b3133d3ea77c62dbb1bb2a
Carlos Eduardo Moura Abe <carlosabe05@gmail.com>
2025-10-03 08:37:30 -0300

    salvando modificações não salvas

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const tarefasRouter = require("./routes/tarefaRouter");
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/tarefas", tarefasRouter);
 
-app.use('/', indexRouter);
 
 module.exports = app;
